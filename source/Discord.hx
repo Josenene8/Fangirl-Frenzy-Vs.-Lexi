64315dadce328d4d9d0bb4b6a8c104b259466df1
Dxgamer <95672691+Amogus124@users.noreply.github.com>
2022-03-21 06:41:02 -0300

    update Discord.hx

--- source/Discord.hx
+++ source/Discord.hx
@@ -1,7 +1,9 @@
 package;
 
+#if desktop
 import Sys.sleep;
 import discord_rpc.DiscordRpc;
+#end
 
 #if LUA_ALLOWED
 import llua.Lua;
@@ -12,14 +14,13 @@ using StringTools;
 
 class DiscordClient
 {
+	public static var isInitialized:Bool = false;
 	public function new()
 	{
+                #if desktop
 		trace("Discord Client starting...");
 		DiscordRpc.start({
-			clientID: "930953879074512946",
-			//930953879074512946
-			//joe dont let your bitch ass self forget too add this back
-			//okay we are good
+			clientID: "863222024192262205",
 			onReady: onReady,
 			onError: onError,
 			onDisconnected: onDisconnected
@@ -34,21 +35,26 @@ class DiscordClient
 		}
 
 		DiscordRpc.shutdown();
+                #end
 	}
 	
 	public static function shutdown()
 	{
+                #if desktop
 		DiscordRpc.shutdown();
+                #end
 	}
 	
 	static function onReady()
 	{
+                #if desktop
 		DiscordRpc.presence({
 			details: "In the Menus",
 			state: null,
-			largeImageKey: 'largeimagekey',
-			largeImageText: "Fangirl Frenzy"
+			largeImageKey: 'icon',
+			largeImageText: "Psych Engine"
 		});
+                #end
 	}
 
 	static function onError(_code:Int, _message:String)
@@ -63,15 +69,19 @@ class DiscordClient
 
 	public static function initialize()
 	{
+                #if desktop
 		var DiscordDaemon = sys.thread.Thread.create(() ->
 		{
 			new DiscordClient();
 		});
 		trace("Discord Client initialized");
+		isInitialized = true;
+                #end
 	}
 
 	public static function changePresence(details:String, state:Null<String>, ?smallImageKey : String, ?hasStartTimestamp : Bool, ?endTimestamp: Float)
 	{
+                #if desktop
 		var startTimestamp:Float = if(hasStartTimestamp) Date.now().getTime() else 0;
 
 		if (endTimestamp > 0)
@@ -82,13 +92,14 @@ class DiscordClient
 		DiscordRpc.presence({
 			details: details,
 			state: state,
-			largeImageKey: 'largeimagekey',
-			largeImageText: "Fangirl Frenzy",
+			largeImageKey: 'icon',
+			largeImageText: "Engine Version: " + MainMenuState.psychEngineVersion,
 			smallImageKey : smallImageKey,
 			// Obtained times are in milliseconds so they are divided so Discord can use it
 			startTimestamp : Std.int(startTimestamp / 1000),
-            endTimestamp : Std.int(endTimestamp / 1000)
+                        endTimestamp : Std.int(endTimestamp / 1000)
 		});
+                #end
 
 		//trace('Discord RPC Updated. Arguments: $details, $state, $smallImageKey, $hasStartTimestamp, $endTimestamp');
 	}

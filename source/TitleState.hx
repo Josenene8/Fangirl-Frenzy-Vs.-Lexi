98ae932bc18588eb3e353bc83afd753b01b90328
Dxgamer <95672691+Amogus124@users.noreply.github.com>
2022-03-21 05:57:31 -0300

    update TitleState.hx

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -73,7 +73,6 @@ class TitleState extends MusicBeatState
 		}
 
 		//Gonna finish this later, probably
-		#end
 		FlxG.game.focusLostFramerate = 60;
 		FlxG.sound.muteKeys = muteKeys;
 		FlxG.sound.volumeDownKeys = volumeDownKeys;
